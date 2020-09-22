42a104fc9b51deed268030b1d4b5e882ea5d4d60
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-22 16:23:19 +0600

    burj-al-arab sign in done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,36 @@
 import React, { useContext } from 'react';
-
-
+import * as firebase from 'firebase/app';
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
 const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // The signed-in user info.
+            const { displayName, email } = result.user;
+            var signedInUser = { name: displayName, email }
+            console.log(signedInUser);
+            setLoggedInUser(signedInUser);
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+
+        });
+    }
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
         </div>
     );
 };
